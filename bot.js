175c18e54e052d0e21688a2f18e1d83e9579840c
D34dlyK!$$ <48548823+D34dlyK1ss@users.noreply.github.com>
2020-02-28 22:03:19 +0000

    Update bot.js

--- bot.js
+++ bot.js
@@ -150,7 +150,7 @@ bot.on('message', message => {
   }
 
   if (message.content.startsWith(`${prefix}say`)) {
-    message.channel.deleteMessage();
+    message.channel.delete();
     message.channel.send(custom);
   }
 
@@ -191,19 +191,17 @@ bot.on('message', message => {
   }
 
   if (message.content.startsWith(`${prefix}clear`)) {
+    message.channel.delete();
     if (!message.member.hasPermission("MANAGE_MESSAGES")) {
-      message.channel.deleteMessage();
       message.channel.send("Não tens permissão para usar este comando! :anger:").then(msg => msg.delete(3000));
     }
     else {
       if (custom == 0)
       {
-        message.channel.deleteMessage();
         message.channel.send("Tens de definir o número de mensagens que queres apagar!").then(msg => msg.delete(3000));
       }
       else
       {
-        message.channel.deleteMessage();
         message.channel.bulkDelete(custom).then(() => {
         message.channel.send(custom + " mensagens foram apagadas!").then(msg => msg.delete(3000));
         });
