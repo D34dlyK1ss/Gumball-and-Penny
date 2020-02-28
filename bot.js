56e2dcd787199cb2f44df1fc6c116cf853914159
D34dlyK!$$ <48548823+D34dlyK1ss@users.noreply.github.com>
2020-02-28 13:18:24 +0000

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,42 +1,40 @@
 const Discord = require('discord.js');
+const {prefix, token} = require('./config.json');
 const bot = new Discord.Client();
 
-bot.on('ready', () => {
-    console.log('Preparados! ^^');
-});
+bot.once('ready', () => {
+    console.log('Preparados! ^^')
+    bot.user.setGame("JavaScript");
+})
 
-bot.login('Njc5MDQxNTQ4OTU1OTQyOTE0.XleH3g.VKU3TFaCfwlOorzjm4LlS67V5Mg');
+bot.login(token);
 
 bot.on('message', message => {
+    
+    var custom = '';
+    var jojo = '';
+    var last = '';
 
-    if (message.content.startsWith('+')) {
-        var custom = '';
-        var jojo = '';
-        var last = '';
-        
-        args = args.splice(1);
-
-        switch(command) {
-            case 'commands':
-                custom = args.join(" ");
-                if (custom == '')
-                {
-                	message.channel.send('Todos os nossos comandos! :video_game:\n```\ncommands\nfact\ninvite\nping\nrandom\ssay\nwhichjojo```Para saberes mais sobre algum comando usa `+commands [nome do comando]`.\n\nP.S.:Existem uns quantos secretos :smiling_imp:');
-                }
-                else
-                {
-                    if (custom == 'commands')
-                    {
-                       message.channel.send('Se não sabes, soubesses! :unamused:');
-                    }
+	if (message.content.startsWith(`${prefix}commands`)) {
+		custom = args.join(" ");		
+		if (custom == '')
+		{
+			message.channel.send('Todos os nossos comandos! :video_game:\n```\ncommands\nfact\ninvite\nping\nrandom\ssay\nwhichjojo```Para saberes mais sobre algum comando usa `+commands [nome do comando]`.\n\nP.S.:Existem uns quantos secretos :smiling_imp:');
+		}
+		else
+		{
+            if (custom == 'commands')
+            {
+               message.channel.send('Se não sabes, soubesses! :unamused:');
+            }
                     
-                    if (custom == 'invite')
-                    {
-                         message.channel.send('O `invite` mostra o link para nos convidares para um dos teus servidores!');
-                    }
-                    
-                    if (custom == 'ping')
-                    {
+            if (custom == 'invite')
+            {
+                message.channel.send('O `invite` mostra o link para nos convidares para um dos teus servidores!');
+            }
+            
+            if (custom == 'ping')
+            {
                         message.channel.send('Uhm... pong?');
                     }
                     
@@ -55,47 +53,56 @@ bot.on('message', message => {
                          message.channel.send("Com o `whichjojo` diremos qual JoJo de _JoJo's Bizarre Adventure_ és!");
                     }
                 }
-            break;
-            case 'ping':
+    
+            if (message.content.startsWith(`${prefix}ping`)) {
                 custom = args.join(" ");
                  message.channel.send('Pong!');
-            break;
-            case 'fact':
+            }
+    
+            if (message.content.startsWith(`${prefix}fact`)) {
                 var rnd = Math.floor(Math.random() * 7);
                 if (message.member.id == 271737298951995393)
                 {
                 rnd = 0;
                 }
+                
                 if (rnd == 0)
                 {
                    message.channel.send('O Leandro é gay.\nhttps://i.imgur.com/CYTHGhZ.png');
                 }
+                
                 if (rnd == 1)
                 {
                    message.channel.send('A soma de dois números idênticos é igual ao dobro de um deles. :nine: x :nine: = :nine: x :two:');
                 }
+                
                 if (rnd == 2)
                 {
                    message.channel.send('Adolf Hitler foi um político alemão que serviu como líder do Partido Nazista, Chanceler do Reich e Führer da Alemanha Nazista desde 1934 até 1945. Como ditador do Reich Alemão, ele foi o principal instigador da Segunda Guerra Mundial e figura central do Holocausto.\nhttps://upload.wikimedia.org/wikipedia/commons/thumb/e/e1/Hitler_portrait_crop.jpg/260px-Hitler_portrait_crop.jpg');
                 }
+                
                 if (rnd == 3)
                 {
                    message.channel.send('Felix Arvid Ulf Kjellberg, mais conhecido por PewDiePie, é um youtuber sueco com o maior número subscritores no mundo.\nhttps://upload.wikimedia.org/wikipedia/commons/thumb/8/86/PewDiePie_at_PAX_2015_crop.jpg/220px-PewDiePie_at_PAX_2015_crop.jpg');
                 }
+                
                 if (rnd == 4)
                 {
                    message.channel.send('A soma do valor dos lados opostos de um dado tradicional dá sempre a soma de 7. :game_die:');
                 }
+                
                 if (rnd == 5)
                 {
                    message.channel.send('A cada 60 segundos em África, um minuto passa.\nhttps://s2.dmcdn.net/v/N3yL71REyTtHN8eYF/x1080');
                 }
+                
                 if (rnd == 6)
                 {
                    message.channel.send('Ninguém morre por comer uma laranja à noite. :tangerine:');
                 }
-            break;
-            case 'whichjojo':
+            }
+    
+            if (message.content.startsWith(`${prefix}whichjojo`)) {
                 last = message.member.id.slice(-1);
                 if (last == 0)
                 {
@@ -138,34 +145,43 @@ bot.on('message', message => {
                   jojo = 'És o Giorno Giovanna!\nhttps://vignette.wikia.nocookie.net/jjba/images/1/19/Giorno_Giovanna_Anime.png';
                 }
                  message.channel.send(jojo);
-            break;
-            case 'invite':
+            }
+    
+            if (message.content.startsWith(`${prefix}invite`)) {
                  message.channel.send('Convida-nos para o teu server! :grin:\nhttps://discordapp.com/oauth2/authorize?&client_id=679041548955942914&scope=bot&permissions=305523718');
-            break;
-            case 'say':
+            }
+    
+            if (message.content.startsWith(`${prefix}say`)) {
                 custom = args.join(" ");
                 message.channel.deleteMessage();
                 message.channel.send(custom);
-            break;
-            case 'shine':
+            }
+    
+            if (message.content.startsWith(`${prefix}shine`)) {
                 message.channel.send('https://i.imgur.com/qSDP6OC.png')
-            break;
-            case 'boi':
+            }
+    
+            if (message.content.startsWith(`${prefix}boi`)) {
                 message.channel.send('https://i.imgur.com/5TQK5F3.png');
-            break;
-            case 'justmonika':
+            }
+    
+            if (message.content.startsWith(`${prefix}justmonika`)) {
                 message.channel.send('https://i.imgur.com/vpbnsDm.png');
-            break;
-            case 'nou':
+            }
+    
+            if (message.content.startsWith(`${prefix}nou`)) {
                 message.channel.send('https://i.imgur.com/O3WduAq.png');
-            break;
-            case 'e':
+            }
+    
+            if (message.content.startsWith(`${prefix}e`)) {
                 message.channel.send('https://i.imgur.com/sHFZIi5.png');
-            break;
-            case 'hmm':
+            }
+    
+            if (message.content.startsWith(`${prefix}hmm`)) {
                 message.channel.send('https://i.imgur.com/SP5VZVG.jpg');
-            break;
-            case 'random':
+            }
+    
+            if (message.content.startsWith(`${prefix}random`)) {
                 custom = args.join(" ");
                 if (custom == '')
                 {
@@ -176,13 +192,14 @@ bot.on('message', message => {
                     rnd = Math.floor(Math.random() * custom) + 1;
                 }
                 message.channel.send(rnd + '!');
-            break;
-            case 'clear':
+            }
+    
+            if (message.content.startsWith(`${prefix}clear`)) {
                 custom = args.join(" ");
                 message.channel.bulkDelete(custom).then(() => {
                     message.channel.send(custom + " mensagens foram apagadas!").then(msg => msg.delete(3000));
                 });
-            break;
+            }
          }
      }
 });
