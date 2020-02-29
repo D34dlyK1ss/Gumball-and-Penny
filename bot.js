4dda4ce0f9c6e365535172d9b1c299cdf8ec4bec
D34dlyK!$$ <48548823+D34dlyK1ss@users.noreply.github.com>
2020-02-29 17:39:29 +0000

    Update bot.js

--- bot.js
+++ bot.js
@@ -10,9 +10,9 @@ bot.once('ready', () => {
 bot.on('message', message => {
 
   if (message.substring(0, 1) == prefix) {
-        var custom = message.substring(1).split(' ');
-        var command = custom[0];
-        custom = custom.splice(1);
+    var custom = message.string(1).split(' ');
+    var command = custom[0];
+    custom = custom.splice(1);
   }
   
   var jojo = '';
