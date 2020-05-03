3325bba2aed2a4e2430c6082444edf03a2c206dc
rodrigoagsilva <rodrigoagsilva2410@gmail.com>
2020-05-03 19:55:31 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,5 +1,5 @@
 const Discord = require('discord.js');
-const token = require('./config.json');
+const {token} = require('./config.json');
 const bot = new Discord.Client();
 
 const firebaseConfig = {
