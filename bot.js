19e6b7e1ebb5a37e6f32fbcb6cb999125259fd6d
rodrigoagsilva <rodrigoagsilva2410@gmail.com>
2020-05-03 15:35:11 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -13,13 +13,14 @@ const firebaseConfig = {
 };
 
 const admin = require('firebase-admin');
-const functions = require('firebase-functions');
+admin.initializeApp({
+  credential: admin.credential.applicationDefault()
+});
+const db = admin.firestore();
 
 const permfail = 'Não tens permissão para usar este comando! :anger:'
 
 bot.once('ready', () => {
-  admin.initializeApp(functions.config().firebase);
-  let db = admin.firestore();
   console.log('Preparado!\nO prefixo é ' + prefix);
   bot.user.setActivity(prefix + 'help');
 })
