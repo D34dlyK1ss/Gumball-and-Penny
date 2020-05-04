e65f2b6c89fc6ca5f9f18d1834a1764968e0994a
rodrigoagsilva <rodrigoagsilva2410@gmail.com>
2020-05-04 14:22:42 +0100

    Update

--- bot.js
+++ bot.js
@@ -1,49 +1,45 @@
+//Inicializar o bot
 const Discord = require('discord.js');
-const fs = require('fs');
 const config = require('./config.json');
-
+const fs = require('fs');
 const bot = new Discord.Client();
 bot.commands = new Discord.Collection();
-
 let prefix;
 const token = config.token;
 const owner = config.owner;
 
+//Aceder à base de dados
 const firebase = require('firebase/app');
 const FieldValue = require('firebase-admin').firestore.FieldValue;
 const admin = require('firebase-admin');
 const serviceAccount = require('./serviceAccount.json');
-
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
   databaseURL: "https://pap-dc-bot.firebaseio.com"
 });
-
 let db = admin.firestore();
 
+//Leitura dos ficheiros de comandos
 fs.readdir('./commands', (err, files) => {
   if (err){
     console.log(err);
   }
-
   let commandFiles = files.filter(f => f.split('.').pop() === 'js');
-
   if (commandFiles.length === 0) return;
-
   commandFiles.forEach((f, i) => {
     let props = require(`./commands/${f}`);
     bot.commands.set(props.help.name, props);
   });
-
 })
 
+//Ações para quando o bot esitver online
 bot.once('ready', async () => {
-  bot.user.setActivity('dc!help');
-  console.log('Preparado!');
+  bot.user.setActivity('dc!help'); //Definir a atividade, que é demonstrada online
+  console.log('Preparado!'); //Verificar se o bot está apto para receber mensagens
 });
 
+//Ações para quando o bot receber uma mensagem
 bot.on('message', message => {
-
   db.collection('servidores').doc(message.guild.id).get().then((query) => {
     if (query.exists){
       prefix = query.data().prefix;
@@ -56,17 +52,21 @@ bot.on('message', message => {
     let command = array[0];
     let args = array.slice(1);
 
+    console.log(`command = ${command}`);
+    console.log(`args = ${args}`);
+
     if (!command.startsWith(prefix)) return;
 
     if (bot.commands.get(command.slice(prefix.length))){
       let cmd = bot.commands.get(command.slice(prefix.length));
       if (cmd){
-        cmd.run(bot, message, args, db);
+        cmd.run(bot, message, command, args, db);
       }
     }
   });
 });
 
+//Ações para quando o bot for adicionado a um novo servidor
 bot.on('guildCreate', async guildData => {
   db.collection('servidores').doc(guildData.id).set({
     'guildID': guildData.id,
@@ -78,4 +78,5 @@ bot.on('guildCreate', async guildData => {
   });
 });
 
+//Para fazer login é usado o respetivo token de aplicação para Discord
 bot.login(token);
---
+++ commands/dm.js
@@ -0,0 +1,24 @@
+module.exports.run = async (bot, message, command, args, db) => {
+    let mention = message.mentions.users.first();
+    let mentionMessage;
+    if (mention == null) {
+        message.delete();
+        message.reply("Não mencnionaste ninguém!").then(msg => msg.delete(3000));
+    }
+    else {
+        if (message.member.userID == 287953505992572929 || message.member.userID == 503009296267608066) {
+            message.delete();
+            let mention2 = String(mention);
+            mentionMessage = message.content.slice(command + 21);
+            mention.sendMessage(mentionMessage);
+        }
+        else {
+            message.delete();
+            message.reply(permfail).then(msg => msg.delete(3000));
+        }
+    }
+}
+
+module.exports.help = {
+    name: 'dm'
+}
