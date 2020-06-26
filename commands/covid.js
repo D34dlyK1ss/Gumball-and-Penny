303da2d94d5e838ec57912f202afa7a7ae229f76
rodrigoagsilva <rodrigoagsilva2410@gmail.com>
2020-06-26 12:31:54 +0100

    Update covid.js

--- commands/covid.js
+++ commands/covid.js
@@ -1,6 +1,16 @@
 var request = require('request');
 
 module.exports.run = async (bot, message, command, args, db) => {
+    let today = new Date().toJSON().slice(0, 10);
+    let yesterday = new Date();
+    yesterday.setDate(yesterday.getDate() - 1);
+
+    let dd = String(yesterday.getDate()).padStart(2, '0');
+    let mm = String(yesterday.getMonth() + 1).padStart(2, '0'); //January is 0!
+    let yyyy = yesterday.getFullYear();
+
+    yesterday = yyyy + '-' + mm + '-' + dd;
+
     args = args.toString();
     if (args == '' || args == null){
         message.channel.send('É necessário mencionar o país!');
@@ -9,7 +19,7 @@ module.exports.run = async (bot, message, command, args, db) => {
         args = args.toUpperCase();
         var options = {
             'method': 'GET',
-            'url': `https://api.covid19api.com/country/${args}?from=2020-06-24T23:59:59Z&to=2020-06-25T00:00:00Z`,
+            'url': `https://api.covid19api.com/country/${args}?from=${yesterday.toString}&to=${today.toString}`,
             'headers': {
             }
         };
