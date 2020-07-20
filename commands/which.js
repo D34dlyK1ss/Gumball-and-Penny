1085c40e4fef737db9bcf2878015cecd389cc08b
d34dlyk1ss <rags.ftjs@gmail.com>
2020-07-20 01:42:30 +0100

    Updated

--- commands/which.js
+++ commands/which.js
@@ -9,34 +9,34 @@ module.exports.run = async (bot, message, command, args, db) => {
   else {
     switch (last) {
       case '0':
-        char = ["o George Joestar I", "o Teru Mikami", "a Mine", "o Mumen Rider", "a Euphemia li Britannia", "a Yumemi Yumemite", "o Ebisu", "o Deus Ex Machina"];
+        char = ["o George Joestar I", "o Teru Mikami", "a Mine", "o Mumen Rider", "a Euphemia li Britannia", "a Yumemi Yumemite", "o Ebisu", "o Deus Ex Machina", "Moeka Kiryuu"];
         break;
       case '1':
-        char = ["a Jolyne Kujo", "o Mihael Keehl", "o Bulat", "o King", "o Xingke Li", "o Kaede Manyuda", "o Daikoku", "o Aru Akise"];
+        char = ["a Jolyne Kujo", "o Mihael Keehl", "o Bulat", "o King", "o Xingke Li", "o Kaede Manyuda", "o Daikoku", "o Aru Akise", "Rumiho Akiha"];
         break;
       case '2':
-        char = ["o Jotaro Kujo", "o L", "o Tatsumi", "o Genos", "o Suzaku Kururugi", "o Ryouta Suzui", "o Yato", "o Keigo Kurusu"];
+        char = ["o Jotaro Kujo", "o L", "o Tatsumi", "o Genos", "o Suzaku Kururugi", "o Ryouta Suzui", "o Yato", "o Keigo Kurusu", "o Itaru Hashida"];
         break;
       case '3':
-        char = ["o Johnny Joestar", "o Touta Matsuda", "a Chelsea", "o Speed-o'-Sound Sonic", "o Kaname Ougi", "a Mary Saotome", "a Nora", "o Yomotsu Hirasaka"];
+        char = ["o Johnny Joestar", "o Touta Matsuda", "a Chelsea", "o Speed-o'-Sound Sonic", "o Kaname Ougi", "a Mary Saotome", "a Nora", "o Yomotsu Hirasaka", "Ruka Urushibara"];
         break;
       case '4':
-        char = ["o Josuke Higashikata!", "a Rem", "o Lubbock", "o Puri-Puri Prisoner", "a Cornelia li Britannia", "a Ririka Momobami", "o Tenjin", "a Tsubaki Kasugano"];
+        char = ["o Josuke Higashikata!", "a Rem", "o Lubbock", "o Puri-Puri Prisoner", "a Cornelia li Britannia", "a Ririka Momobami", "o Tenjin", "a Tsubaki Kasugano", "Maho Hiyajou"];
         break;
       case '5':
-        char = ["o George Joestar II!", "a Naomi Misora", "a Akame", "o Bang", "a Nunnally vi Britannia", "a Yumeko Jabami", "o Kazuma", "o Yukiteru Amano"];
+        char = ["o George Joestar II!", "a Naomi Misora", "a Akame", "o Bang", "a Nunnally vi Britannia", "a Yumeko Jabami", "o Kazuma", "o Yukiteru Amano", "Yuugo Tennouji"];
         break;
       case '6':
-        char = ["o Josuke Higashikata", "o Ryuk", "a Najenda", "a Tatsumaki", "a Shirley Fenette", "a Runa Yomozuki", "o Yukine", "a Yuno Gasai"];
+        char = ["o Josuke Higashikata", "o Ryuk", "a Najenda", "a Tatsumaki", "a Shirley Fenette", "a Runa Yomozuki", "o Yukine", "a Yuno Gasai", "a Mayuri Shiina"];
         break;
       case '7':
-        char = ["o Joseph Joestar", "o Nate River", "a Esdeath", "o Garou", "a Kallen Stadtfeld", "a Itsuki Sumeragi", "a Kofuku", "a Hinata Hino"];
+        char = ["o Joseph Joestar", "o Nate River", "a Esdeath", "o Garou", "a Kallen Stadtfeld", "a Itsuki Sumeragi", "a Kofuku", "a Hinata Hino", "Suzuha Amane"];
         break;
       case '8':
-        char = ["o Johnathan Joestar", "a Misa Amane", "a Leone", "a Fubuki", "a C.C.", "a Kirari Momobami", "a Hiyori Iki", "a Muru Muru"];
+        char = ["o Johnathan Joestar", "a Misa Amane", "a Leone", "a Fubuki", "a C.C.", "a Kirari Momobami", "a Hiyori Iki", "a Muru Muru", "Kurisu Makise"];
         break;
       case '9':
-        char = ["o Giorno Giovanna", "o Light Yagami!", "a Sheele", "o Saitama", "o Lelouch vi Britannia", "a Midari Ikishima", "a Bishamon", "a Minene Uryuu"];
+        char = ["o Giorno Giovanna", "o Light Yagami!", "a Sheele", "o Saitama", "o Lelouch vi Britannia", "a Midari Ikishima", "a Bishamon", "a Minene Uryuu", "o Rintarou Okabe"];
         break;
     }
 
@@ -64,6 +64,9 @@ module.exports.run = async (bot, message, command, args, db) => {
         break;
       case "mirai,nikki":
         message.channel.send(`És ${char[7]}!`, { files: ["images/which/mn (" + last + ").jpg"] });
+        break;
+      case "steins;gate":
+        message.channel.send(`És ${char[8]}!`, { files: ["images/which/sg (" + last + ").jpg"] });
         break;
     }
   }
