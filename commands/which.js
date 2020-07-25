93e214675fdb746d6a10b7057de9f1433f2c3e2d
d34dlyk1ss <rags.ftjs@gmail.com>
2020-07-25 12:44:40 +0100

    Updated

--- commands/which.js
+++ commands/which.js
@@ -9,34 +9,34 @@ module.exports.run = async (bot, message, command, args, db) => {
   else {
     switch (last) {
       case '0':
-        char = ["o George Joestar I", "o Teru Mikami", "a Mine", "o Mumen Rider", "a Euphemia li Britannia", "a Yumemi Yumemite", "o Ebisu", "o Deus Ex Machina", "a Moeka Kiryuu"];
+        char = ["o George Joestar I", "o Teru Mikami", "a Mine", "o Mumen Rider", "a Euphemia li Britannia", "a Yumemi Yumemite", "o Ebisu", "o Deus Ex Machina", "a Moeka Kiryuu", "Rika Furude"];
         break;
       case '1':
-        char = ["a Jolyne Kujo", "o Mihael Keehl", "o Bulat", "o King", "o Xingke Li", "o Kaede Manyuda", "o Daikoku", "o Aru Akise", "a Rumiho Akiha"];
+        char = ["a Jolyne Kujo", "o Mihael Keehl", "o Bulat", "o King", "o Xingke Li", "o Kaede Manyuda", "o Daikoku", "o Aru Akise", "a Rumiho Akiha", "Rena Ryuuguu"];
         break;
       case '2':
-        char = ["o Jotaro Kujo", "o L", "o Tatsumi", "o Genos", "o Suzaku Kururugi", "o Ryouta Suzui", "o Yato", "o Keigo Kurusu", "o Itaru Hashida"];
+        char = ["o Jotaro Kujo", "o L", "o Tatsumi", "o Genos", "o Suzaku Kururugi", "o Ryouta Suzui", "o Yato", "o Keigo Kurusu", "o Itaru Hashida", "Keiichi Maebara"];
         break;
       case '3':
-        char = ["o Johnny Joestar", "o Touta Matsuda", "a Chelsea", "o Speed-o'-Sound Sonic", "o Kaname Ougi", "a Mary Saotome", "a Nora", "o Yomotsu Hirasaka", "o Ruka Urushibara"];
+        char = ["o Johnny Joestar", "o Touta Matsuda", "a Chelsea", "o Speed-o'-Sound Sonic", "o Kaname Ougi", "a Mary Saotome", "a Nora", "o Yomotsu Hirasaka", "o Ruka Urushibara", "Miyo Takano"];
         break;
       case '4':
-        char = ["o Josuke Higashikata!", "a Rem", "o Lubbock", "o Puri-Puri Prisoner", "a Cornelia li Britannia", "a Ririka Momobami", "o Tenjin", "a Tsubaki Kasugano", "a Maho Hiyajou"];
+        char = ["o Josuke Higashikata!", "a Rem", "o Lubbock", "o Puri-Puri Prisoner", "a Cornelia li Britannia", "a Ririka Momobami", "o Tenjin", "a Tsubaki Kasugano", "a Maho Hiyajou", "Ooishi Kuraudo"];
         break;
       case '5':
-        char = ["o George Joestar II!", "a Naomi Misora", "a Akame", "o Bang", "a Nunnally vi Britannia", "a Yumeko Jabami", "o Kazuma", "o Yukiteru Amano", "o Yuugo Tennouji"];
+        char = ["o George Joestar II!", "a Naomi Misora", "a Akame", "o Bang", "a Nunnally vi Britannia", "a Yumeko Jabami", "o Kazuma", "o Yukiteru Amano", "o Yuugo Tennouji", "Shion Sonozaki"];
         break;
       case '6':
-        char = ["o Josuke Higashikata", "o Ryuk", "a Najenda", "a Tatsumaki", "a Shirley Fenette", "a Runa Yomozuki", "o Yukine", "a Yuno Gasai", "a Mayuri Shiina"];
+        char = ["o Josuke Higashikata", "o Ryuk", "a Najenda", "a Tatsumaki", "a Shirley Fenette", "a Runa Yomozuki", "o Yukine", "a Yuno Gasai", "a Mayuri Shiina", "Hanyuu"];
         break;
       case '7':
-        char = ["o Joseph Joestar", "o Nate River", "a Esdeath", "o Garou", "a Kallen Stadtfeld", "a Itsuki Sumeragi", "a Kofuku", "a Hinata Hino", "a Suzuha Amane"];
+        char = ["o Joseph Joestar", "o Nate River", "a Esdeath", "o Garou", "a Kallen Stadtfeld", "a Itsuki Sumeragi", "a Kofuku", "a Hinata Hino", "a Suzuha Amane", "Satoko Houjou"];
         break;
       case '8':
-        char = ["o Johnathan Joestar", "a Misa Amane", "a Leone", "a Fubuki", "a C.C.", "a Kirari Momobami", "a Hiyori Iki", "a Muru Muru", "a Kurisu Makise"];
+        char = ["o Johnathan Joestar", "a Misa Amane", "a Leone", "a Fubuki", "a C.C.", "a Kirari Momobami", "a Hiyori Iki", "a Muru Muru", "a Kurisu Makise", "Mion Sonozaki"];
         break;
       case '9':
-        char = ["o Giorno Giovanna", "o Light Yagami!", "a Sheele", "o Saitama", "o Lelouch vi Britannia", "a Midari Ikishima", "a Bishamon", "a Minene Uryuu", "o Rintarou Okabe"];
+        char = ["o Giorno Giovanna", "o Light Yagami!", "a Sheele", "o Saitama", "o Lelouch vi Britannia", "a Midari Ikishima", "a Bishamon", "a Minene Uryuu", "o Rintarou Okabe", "Mamoru Akasaka"];
         break;
     }
 
@@ -68,6 +68,9 @@ module.exports.run = async (bot, message, command, args, db) => {
       case "steins;gate":
         message.channel.send(`És ${char[8]}!`, { files: ["images/which/sg (" + last + ").jpg"] });
         break;
+      case "higurashi":
+        message.channel.send(`És ${char[8]}!`, { files: ["images/which/higurashi (" + last + ").jpg"] });
+        break;
     }
   }
 }
@@ -75,6 +78,6 @@ module.exports.run = async (bot, message, command, args, db) => {
 module.exports.help = {
   name: "which",
   category: "Diversão",
-  description: "Com este comando podes saber que personagem de anime és!\nAnimes disponíveis: `akame ga kill`, `code geass`, `death note`, `jojo`, `kakegurui`, `mirai nikki`, `noragami`, `one punch man`, `steins;gate`",
+  description: "Com este comando podes saber que personagem de anime és!\nAnimes disponíveis: `akame ga kill`, `code geass`, `death note`, `higurashi` , `jojo`, `kakegurui`, `mirai nikki`, `noragami`, `one punch man`, `steins;gate`",
   usage: "`+which [anime]`"
 }
