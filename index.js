faef9fdb2c13310a4532bb1080f64caa47195891
d34dlyk1ss <rags.ftjs@gmail.com>
2020-07-29 20:26:16 +0100

    Updated

--- index.js
+++ index.js
@@ -37,18 +37,18 @@ fs.readdir('./commands', (err, files) => {
 bot.once('ready', async () => {
   console.log('Preparados!');
 
-  const currentdate = new Date();
-  const relationship = new Date(2019, 11, 28);
-  const mili = currentdate - relationship;
-  const months = Math.round(mili / 2629746000);
-  const years = Math.round(mili / 31536000000);
-  const lilly = bot.users.resolve("503009296267608066");
-  const ruru = bot.users.resolve("287953505992572929");
-
   bot.user.setActivity("+help");
+
+  const currentdate = new Date(),
+        relationship = new Date(2019, 11, 28),
+        mili = currentdate - relationship;
+
+  const months = Math.round(mili / 2629746000),
+        years = Math.round(mili / 31536000000);
+
   let j = schedule.scheduleJob("0 14 28 * *", function () {
-    lilly.send(`:tada: Parabéns Lilly! Completaste ${months} meses com o teu Ruru! :purple_heart:\nhttps://i.imgur.com/clrwrEk.gif`);
-    ruru.send(`:tada: Parabéns Ruru! Completaste ${months} meses com a tua Lilly! :purple_heart:\nhttps://i.imgur.com/clrwrEk.gif`);
+    bot.users.resolve("503009296267608066").send(`:tada: Parabéns Lilly! Completaste ${months} meses com o teu Ruru! :purple_heart:\nhttps://i.imgur.com/clrwrEk.gif`);
+    bot.users.resolve("287953505992572929").send(`:tada: Parabéns Ruru! Completaste ${months} meses com a tua Lilly! :purple_heart:\nhttps://i.imgur.com/clrwrEk.gif`);
   });
 });
 
@@ -59,85 +59,81 @@ bot.on('message', message => {
   if (message.author.bot) return;
 
   let prefix;
-
-  db.collection('servidores').doc(message.guild.id).get().then((query) => {
-    if (query.exists){
-      prefix = query.data().prefix; //Obter o prefixo definido para o servidor
+  let ref = db.collection('servidores').doc(message.guild.id);
+  ref.get().then(doc => {
+    if (doc.exists){
+      prefix = doc.data().prefix; //Obter o prefixo definido para o servidor
     }
   }).then(() => {
-    let array = message.content.split(' ');
-    let command = array[0];
-    let args = array.slice(1);
+    let array = message.content.split(' '),
+        command = array[0],
+        args = array.slice(1);
     
     if (!command.startsWith(prefix)) return; //Ignorar mensagens que não começam com o prefixo
 
     if (bot.commands.get(command.slice(prefix.length))){
       let cmd = bot.commands.get(command.slice(prefix.length));
       if (cmd){
-        cmd.run(bot, message, command, args, db); //Correr as ações do comando
+        cmd.run(bot, message, command, args, db, ref); //Correr as ações do comando
       }
     }
   });
 
-  let gName = db.collection('servidores').doc(message.guild.id).get('guildName');
-  let oName = db.collection('servidores').doc(message.guild.id).get('guildOwner');
-  let oID = db.collection('servidores').doc(message.guild.id).get('guildOwnerID');
-  let mCount = db.collection('servidores').doc(message.guild.id).get('memberCount');
+  let gName = ref.get('guildName'),
+      oName = ref.get('guildOwner'),
+      oID = ref.get('guildOwnerID'),
+      mCount = ref.get('memberCount');
 
   //Atualizar o Nome do servidor
   if (message.guild.name != gName) {
-    db.collection('servidores').doc(message.guild.id).update({
+    ref.update({
       guildName: message.guild.name
     });
   }
 
   //Atualizar o nome do proprietário do servidor
   if (message.guild.owner.user.username != oName) {
-    db.collection('servidores').doc(message.guild.id).update({
+    ref.update({
       guildOwner: message.guild.owner.user.username
     });
   }
 
   //Atualizar o ID do proprietário do servidor
   if (message.guild.owner.user.id != oID) {
-    db.collection('servidores').doc(message.guild.id).update({
+    ref.update({
       guildOwnerID: message.guild.owner.user.id
     });
   }
 
   //Atualizar o número de membros do servidor
   if (message.guild.members != mCount) {
-    db.collection('servidores').doc(message.guild.id).update({
+    ref.update({
       memberCount: message.guild.memberCount
     });
   }
 
-  if (message.content == 'shine') {
-    message.channel.send({ files: ["images/shine.png"] });
-  }
-
-  if (message.content == 'boi') {
-    message.channel.send({ files: ["images/boi.png"] });
-  }
-
-  if (message.content == 'just monika') {
-    message.channel.send({ files: ["images/monika.png"] });
-  }
-
-  if (message.content == 'no u') {
-    message.channel.send({ files: ["images/no u.png"] });
-  }
-
-  if (message.content == 'E') {
-    message.channel.send({ files: ["images/E.png"] });
-  }
-
-  if (message.content == 'hmm') {
-    message.channel.send({ files: ["images/hmm.jpg"] });
-  }
-
-  if (message.content == 'noice') {
-    message.channel.send({ files: ["images/noice.png"] });
+  switch (message.content){
+    case 'shine':
+      message.channel.send({ files: ["images/shine.png"] });
+      break;
+    case 'boi':
+      message.channel.send({ files: ["images/boi.png"] });
+      break;
+    case 'just monika':
+      message.channel.send({ files: ["images/just monika.png"] });
+      break;
+    case 'no u':
+      message.channel.send({ files: ["images/no u.png"] });
+      break;
+    case 'E':
+      message.channel.send({ files: ["images/E.png"] });
+      break;
+    case 'hmm':
+      message.channel.send({ files: ["images/hmm.png"] });
+      break;
+    case 'noice':
+      message.channel.send({ files: ["images/noice.png"] });
+      break;
   }
 });
 
