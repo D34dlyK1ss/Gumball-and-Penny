f02be11d573c146caaf7b631d988d2b90ba5f1c4
d34dlyk1ss <rags.ftjs@gmail.com>
2020-07-31 13:03:34 +0100

    Updated

--- index.js
+++ index.js
@@ -1,150 +1,164 @@
-const Discord = require('discord.js'); //Biblioteca do Discord.js
-const bot = new Discord.Client(); //Cliente
-const config = require('./config.json'); //Propriedades default do bot
-const token = config.token; //Token do bot para autenticação
-const botOwner = config.botOwner; //Discord ID do proprietário do bot
-const schedule = require('node-schedule');//Biblioteca para horários
-const fs = require('fs'); //Biblioteca para sistema de ficheiros
+// Biblioteca do Discord.js
+const Discord = require('discord.js');
+// Cliente
+const bot = new Discord.Client();
+// Propriedades default do bot
+const config = require('./config.json');
+// Token do bot para autenticação
+const token = config.token;
+// Biblioteca para horários
+const schedule = require('node-schedule');
+// Biblioteca para sistema de ficheiros
+const fs = require('fs');
 
-bot.commands = new Discord.Collection(); //Classe de utilidade 'Collection' do Discord.js
+// Classe de utilidade 'Collection' do Discord.js
+bot.commands = new Discord.Collection();
 
-const admin = require('firebase-admin'); //Acesso de administrador à BD
-const serviceAccount = require('./serviceAccountKey.json'); //Chaves de autenticação à BD
+// Acesso de administrador à BD
+const admin = require('firebase-admin');
+// Chaves de autenticação à BD
+const serviceAccount = require('./serviceAccountKey.json');
 admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount), //Autenticação à BD
-  databaseURL: 'https://gumball-and-penny.firebaseio.com'
+	// Autenticação à BD
+	credential: admin.credential.cert(serviceAccount),
+	databaseURL: 'https://gumball-and-penny.firebaseio.com',
 });
 
 const db = admin.firestore();
 
-//Leitura dos ficheiros de comandos
+// Leitura dos ficheiros de comandos
 
-let commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
 if (commandFiles.length === 0) return;
 for (const file of commandFiles) {
-  const props = require(`./commands/${file}`);
-  bot.commands.set(props.name, props);
+	const props = require(`./commands/${file}`);
+	bot.commands.set(props.name, props);
 }
 
-//Uma vez que o bot está ativo:
+// Uma vez que o bot está ativo:
 bot.once('ready', async () => {
-  console.log('Preparados!');
+	console.log('Preparados!');
 
-  bot.user.setActivity("+help");
+	bot.user.setActivity('+help');
 
-  const currentdate = new Date(),
-        relationship = new Date(2019, 11, 28),
-        mili = currentdate - relationship;
+	const currentdate = new Date(),
+		relationship = new Date(2019, 11, 28),
+		mili = currentdate - relationship;
 
-  const months = Math.round(mili / 2629746000),
-        years = Math.round(mili / 31536000000);
+	const months = Math.round(mili / 2629746000),
+		// eslint-disable-next-line no-unused-vars
+		years = Math.round(mili / 31536000000);
 
-  let j = schedule.scheduleJob("0 14 28 * *", function () {
-    bot.users.resolve("503009296267608066").send(`:tada: Parabéns Lilly! Completaste ${months} meses com o teu Ruru! :purple_heart:\nhttps://i.imgur.com/clrwrEk.gif`);
-    bot.users.resolve("287953505992572929").send(`:tada: Parabéns Ruru! Completaste ${months} meses com a tua Lilly! :purple_heart:\nhttps://i.imgur.com/clrwrEk.gif`);
-  });
+	schedule.scheduleJob('0 14 28 * *', function() {
+		bot.users.resolve('503009296267608066').send(`:tada: Parabéns Lilly! Completaste ${months} meses com o teu Ruru! :purple_heart:\nhttps://i.imgur.com/clrwrEk.gif`);
+		bot.users.resolve('287953505992572929').send(`:tada: Parabéns Ruru! Completaste ${months} meses com a tua Lilly! :purple_heart:\nhttps://i.imgur.com/clrwrEk.gif`);
+	});
 });
 
-//Ações para quando o bot receber uma mensagem
+// Ações para quando o bot receber uma mensagem
 bot.on('message', message => {
 
-  let prefix;
-  const ref = db.collection('servidores').doc(message.guild.id);
+	let prefix;
+	const ref = db.collection('servidores').doc(message.guild.id);
 
-  //Ignorar mensagens privadas e mensagens de outros bots
-  if (message.channel.type === 'dm' || message.author.bot) return;
+	// Ignorar mensagens privadas e mensagens de outros bots
+	if (message.channel.type === 'dm' || message.author.bot) return;
 
-  ref.get().then(doc => {
-    if (doc.exists) {
-      prefix = doc.get("prefix"); //Obter o prefixo definido para o servidor
-    }
-  }).then(() => {
-    if (!message.content.startsWith(prefix)) return; //Ignorar mensagens que não começam com o prefixo
+	ref.get().then(doc => {
+		if (doc.exists) {
+			// Obter o prefixo definido para o servidor
+			prefix = doc.get('prefix');
+		}
+	}).then(() => {
+		// Ignorar mensagens que não começam com o prefixo
+		if (!message.content.startsWith(prefix)) return;
 
-    let array = message.content.split(' ');
-    let command = array[0].slice(prefix.length);
-    let args = array.slice(1);
+		const array = message.content.split(' ');
+		const command = array[0].slice(prefix.length);
+		const args = array.slice(1);
 
-    if (!bot.commands.has(command)) return; //Ignorar mensagem se o bot não tiver tal comando
+		// Ignorar mensagem se o bot não tiver tal comando
+		if (!bot.commands.has(command)) return;
 
-    try {
-      bot.commands.get(command).execute(bot, message, command, args, db);
-    } catch (err) {
-      console.error(err);
-      message.reply('ocorreu um erro ao tentar executar esse comando!');
-    }
-  });
+		try {
+			bot.commands.get(command).execute(bot, message, command, args, db);
+		}
+		catch (err) {
+			console.error(err);
+			message.reply('ocorreu um erro ao tentar executar esse comando!');
+		}
+	});
 
-  let gName = ref.get('guildName'),
-      oName = ref.get('guildOwner'),
-      oID = ref.get('guildOwnerID'),
-      mCount = ref.get('memberCount');
+	const gName = ref.get('guildName'),
+		oName = ref.get('guildOwner'),
+		oID = ref.get('guildOwnerID'),
+		mCount = ref.get('memberCount');
 
-  //Atualizar o nome do servidor
-  if (message.guild.name != gName) {
-    ref.update({
-      guildName: message.guild.name
-    });
-  }
+	// Atualizar o nome do servidor
+	if (message.guild.name != gName) {
+		ref.update({
+			guildName: message.guild.name,
+		});
+	}
 
-  //Atualizar o nome do proprietário do servidor
-  if (message.guild.owner.user.username != oName) {
-    ref.update({
-      guildOwner: message.guild.owner.user.username
-    });
-  }
+	// Atualizar o nome do proprietário do servidor
+	if (message.guild.owner.user.username != oName) {
+		ref.update({
+			guildOwner: message.guild.owner.user.username,
+		});
+	}
 
-  //Atualizar o ID do proprietário do servidor
-  if (message.guild.owner.user.id != oID) {
-    ref.update({
-      guildOwnerID: message.guild.owner.user.id
-    });
-  }
+	// Atualizar o ID do proprietário do servidor
+	if (message.guild.owner.user.id != oID) {
+		ref.update({
+			guildOwnerID: message.guild.owner.user.id,
+		});
+	}
 
-  //Atualizar o número de membros do servidor
-  if (message.guild.members != mCount) {
-    ref.update({
-      memberCount: message.guild.memberCount
-    });
-  }
+	// Atualizar o número de membros do servidor
+	if (message.guild.members != mCount) {
+		ref.update({
+			memberCount: message.guild.memberCount,
+		});
+	}
 
-  //Responder de acordo com o conteúdo da mensagem lida
-  switch (message.content){
-    case 'shine':
-      message.channel.send({ files: ["images/shine.png"] });
-      break;
-    case 'boi':
-      message.channel.send({ files: ["images/boi.png"] });
-      break;
-    case 'just monika':
-      message.channel.send({ files: ["images/just monika.png"] });
-      break;
-    case 'no u':
-      message.channel.send({ files: ["images/no u.png"] });
-      break;
-    case 'E':
-      message.channel.send({ files: ["images/E.png"] });
-      break;
-    case 'hmm':
-      message.channel.send({ files: ["images/hmm.png"] });
-      break;
-    case 'noice':
-      message.channel.send({ files: ["images/noice.png"] });
-      break;
-  }
+	// Responder de acordo com o conteúdo da mensagem lida
+	switch (message.content) {
+	case 'shine':
+		message.channel.send({ files: ['images/shine.png'] });
+		break;
+	case 'boi':
+		message.channel.send({ files: ['images/boi.png'] });
+		break;
+	case 'just monika':
+		message.channel.send({ files: ['images/just monika.png'] });
+		break;
+	case 'no u':
+		message.channel.send({ files: ['images/no u.png'] });
+		break;
+	case 'E':
+		message.channel.send({ files: ['images/E.png'] });
+		break;
+	case 'hmm':
+		message.channel.send({ files: ['images/hmm.png'] });
+		break;
+	case 'noice':
+		message.channel.send({ files: ['images/noice.png'] });
+		break;
+	}
 });
 
-//Quando o bot for adicionado a um novo servidor, são armazenados dados do mesmo
+// Quando o bot for adicionado a um novo servidor, são armazenados dados do mesmo
 bot.on('guildCreate', async guildData => {
-  db.collection('servidores').doc(guildData.id).set({
-    'guildID': guildData.id,
-    'guildName': guildData.name,
-    'guildOwner': guildData.owner.user.username,
-    'guildOwnerID': guildData.owner.user.id,
-    'memberCount': guildData.memberCount,
-    'prefix': '+'
-  });
+	db.collection('servidores').doc(guildData.id).set({
+		'guildID': guildData.id,
+		'guildName': guildData.name,
+		'guildOwner': guildData.owner.user.username,
+		'guildOwnerID': guildData.owner.user.id,
+		'memberCount': guildData.memberCount,
+		'prefix': '+',
+	});
 });
 
-//Autenticação do bot
+// Autenticação do bot
 bot.login(token);
