2f0ed8f957a484fe91c95f19b737e80fc115d3d8
d34dlyk1ss <rags.ftjs@gmail.com>
2020-08-02 19:04:56 +0100

    Updated

---
+++ commands/couple.js
@@ -0,0 +1,12 @@
+/* eslint-disable no-empty-function */
+/* eslint-disable no-unused-vars */
+module.exports = {
+	name: 'couple',
+	category: 'Diversão',
+	description: 'Comando para teste',
+	usage: '`+couple [@membro] [opcional - @membro]`',
+
+	execute(bot, message, command, args, db) {
+
+	},
+};
