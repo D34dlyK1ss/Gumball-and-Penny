07de9732a0bc6cd3102717a44c1c25ad4c8b9028
d34dlyk1ss <rags.ftjs@gmail.com>
2020-08-02 22:13:34 +0100

    Updated

--- commands/couple.js
+++ commands/couple.js
@@ -8,16 +8,15 @@ module.exports = {
 
 	execute(bot, message, command, args, db) {
 		const other = message.mentions.users.first();
-		const last = message.member.id.slice(-1);
-		let otherLast,
-			number;
 
 		if (other == null) {
 			message.reply('não mencionaste ninguém!');
 		}
 		else {
-			otherLast = other.id.slice(-1);
-			number = `${last + otherLast}0`;
+			const last = Number(`${message.member.id.slice(-1)}`),
+				otherLast = Number(`${other.id.slice(-1)}`);
+
+			const number = `${last + otherLast}0`;
 
 			if (other == message.author) {
 				message.reply('não podes usar este comando em ti!');
