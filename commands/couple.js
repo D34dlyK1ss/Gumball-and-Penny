6bc2ccaa268ac5a5b72204286f71d17483409975
d34dlyk1ss <rags.ftjs@gmail.com>
2020-08-02 21:03:50 +0100

    Updated

--- commands/couple.js
+++ commands/couple.js
@@ -1,12 +1,33 @@
 /* eslint-disable no-empty-function */
 /* eslint-disable no-unused-vars */
 module.exports = {
-	name: 'couple',
+	name: 'match',
 	category: 'Diversão',
-	description: 'Comando para teste',
-	usage: '`+couple [@membro] [opcional - @membro]`',
+	description: 'Verifica se és compatível com alguém!',
+	usage: '`+match [@membro]`',
 
 	execute(bot, message, command, args, db) {
+		const other = message.mentions.users.first();
+		const last = message.member.id.slice(-1);
+		let otherLast,
+			number;
 
+		if (other == null) {
+			message.reply('não mencionaste ninguém!');
+		}
+		else {
+			otherLast = other.id.slice(-1);
+
+			if (last >= 5 && otherLast >= 5) number = `${otherLast}${last}`;
+			else if (last >= 5 && otherLast <= 5) number = `${last}${otherLast}`;
+			else if (last <= 5 && otherLast >= 5) number = `${last}${otherLast}`;
+			else number = `${otherLast}${last}`;
+
+
+			if (other == message.author) message.reply('não podes usar este comando em ti!');
+			else if (other == bot.user) message.channel.send('Hey, nós já temos um par! 😠');
+			else if (other.bot) message.reply('iss onão vai funcionar com um bot! 😂');
+			else message.channel.send(`Vocês têm **${number}%** de chance de dar certo!`);
+		}
 	},
 };
