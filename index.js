6a19c8d63d650007fa1baa5b75a6c38719cddb46
d34dlyk1ss <rags.ftjs@gmail.com>
2020-08-04 14:37:00 +0100

    Updated

--- index.js
+++ index.js
@@ -91,7 +91,7 @@ bot.on('message', message => {
 
 				db.collection('perfis').doc(message.author.id).update({
 					xp: parseInt(newXP),
-					level: Math.floor(newXP / ((level + 1) * 30)),
+					level: Math.floor(newXP / ((newLevel + 1) * 30)),
 				});
 
 				if (newLevel > level) {
