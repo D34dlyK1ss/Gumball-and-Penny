79873f21b76a71c28db227b267b21540443e0d97
d34dlyk1ss <rags.ftjs@gmail.com>
2020-08-11 00:23:14 +0100

    Updated

--- commands/profile.js
+++ commands/profile.js
@@ -149,7 +149,7 @@ module.exports = {
 					ctx.fillText(`${nick}`, 375, 80);
 
 					ctx.font = '12px Helvetica';
-					ctx.fillText(`${desc}`, 400, 140);
+					ctx.fillText(`${desc}`, 398, 140);
 
 					ctx.font = '14px Helvetica';
 					ctx.textAlign = 'left';
