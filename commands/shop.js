74af01fb5ff4bd2d0f5b9064ec6a47bd795e2445
d34dlyk1ss <rags.ftjs@gmail.com>
2020-08-12 22:01:20 +0100

    Updated

--- commands/shop.js
+++ commands/shop.js
@@ -10,33 +10,29 @@ module.exports = {
 	usage: '`+shop`',
 
 	execute(bot, message, command, args, db) {
-		const mainShop = new MessageEmbed()
-				.setAuthor(`${bot.user.tag}`, `${bot.user.displayAvatarURL()}`)
-				.setColor('#8000ff')
-				.setTitle('Loja')
-				.setDescription(`Bem vindo à nossa loja!
+		const mainEmbed = new MessageEmbed()
+			.setAuthor(`${bot.user.tag}`, `${bot.user.displayAvatarURL()}`)
+			.setColor('#8000ff')
+			.setTitle('Loja')
+			.setDescription(`Bem vindo à nossa loja!
 			Aqui poderás comprar algumas coisas com o dinheiro que acumulaste até agora.
 			Usa \`+shop [número]\` para selecionares uma categoria.`)
-				.addFields(
-					{ name: 'A - Imagens de Fundo', value: '\u200B' },
-					{ name: 'B - HUD', value: '\u200B' },
-				)
-				.setFooter(`${message.author.tag}`, `${message.author.displayAvatarURL()}`),
-			aShop = new MessageEmbed()
-				.setAuthor(`${bot.user.tag}`, `${bot.user.displayAvatarURL()}`)
-				.setColor('#8000ff')
-				.setTitle('Loja')
-				.setDescription('Imagens de Fundo')
-				.addFields(
+			.setFooter(`${message.author.tag}`, `${message.author.displayAvatarURL()}`)
+			.addFields(
+				{ name: 'A - Imagens de Fundo', value: '\u200B' },
+				{ name: 'B - HUD', value: '\u200B' },
+			);
+		const aEmbed = new MessageEmbed(mainEmbed)
+				.spliceFields(0, 2, [
 					{ name: 'N/A', value: '\u200B' },
-				)
-				.setFooter(`${message.author.tag}`, `${message.author.displayAvatarURL()}`),
-			bShop = new MessageEmbed()
-				.setAuthor(`${bot.user.tag}`, `${bot.user.displayAvatarURL()}`)
-				.setColor('#8000ff')
-				.setTitle('Loja')
-				.setDescription('HUD')
+				])
+				.setTitle('Loja - Imagens de Fundo')
+				.setDescription('`+shop background [item]`')
 				.addFields(
+
+				),
+			bEmbed = new MessageEmbed(mainEmbed)
+				.spliceFields(0, 2, [
 					{ name: 'Amarelo', value: '¤1500', inline: true },
 					{ name: 'Azul', value: '¤1500', inline: true },
 					{ name: 'Cinzento', value: '¤1500', inline: true },
@@ -48,10 +44,11 @@ module.exports = {
 					{ name: 'Roxo', value: '¤1500', inline: true },
 					{ name: 'Verde', value: '¤1500', inline: true },
 					{ name: 'Vermelho', value: '¤1500', inline: true },
-				)
-				.setFooter(`${message.author.tag}`, `${message.author.displayAvatarURL()}`);
+				])
+				.setTitle('Loja - HUD')
+				.setDescription('`+shop hud [item]`');
 
-		message.channel.send(mainShop).then(async msg => {
+		message.channel.send(mainEmbed).then(async msg => {
 			try {
 				msg.react('🇦');
 				await msg.react('🇧');
@@ -72,25 +69,25 @@ module.exports = {
 
 			a.on('collect', async reaction => {
 				if (onMain == false) return;
-				msg.reactions.removeAll();
-				msg.edit(aShop);
 				onMain = false;
+				msg.reactions.removeAll();
+				msg.edit(aEmbed);
 				msg.react('↩️');
 			});
 
 			b.on('collect', async reaction => {
 				if (onMain == false) return;
-				msg.reactions.removeAll();
-				msg.edit(bShop);
 				onMain = false;
+				msg.reactions.removeAll();
+				msg.edit(bEmbed);
 				msg.react('↩️');
 			});
 
 			main.on('collect', async reaction => {
 				if (onMain == true) return;
-				msg.reactions.removeAll();
-				msg.edit(mainShop);
 				onMain = true;
+				msg.reactions.removeAll();
+				msg.edit(mainEmbed);
 				msg.react('🇦');
 				await msg.react('🇧');
 			});
