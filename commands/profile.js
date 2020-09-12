8bc7c8a78f8e360df3f2829d28de7f990e6e223e
D34dlyK1ss <rags.ftjs@gmail.com>
2020-09-12 19:06:09 +0100

    Updated

--- commands/profile.js
+++ commands/profile.js
@@ -223,7 +223,7 @@ module.exports = {
 					const vip = doc.get('vip') || false;
 					if (vip == true) {
 						const crown = await loadImage('./images/profile/crown.png');
-						ctx.drawImage (crown, 20, 0, 50, 50);
+						ctx.drawImage (crown, 16, 0, 50, 50);
 					}
 
 					const avatar = await loadImage(user.displayAvatarURL({ format: 'jpg' }));
