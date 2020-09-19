8ae943020f74f5ebaa2a2123ef124d3af1e5e423
D34dlyK1ss <rags.ftjs@gmail.com>
2020-09-19 23:43:46 +0100

    Updated

--- index.js
+++ index.js
@@ -68,7 +68,7 @@ const express = require('express');
 const http = require('http');
 const WebSocket = require('ws');
 
-const port = 5000;
+const port = 6969;
 const server = http.createServer(express);
 const wss = new WebSocket.Server({ server });
 
