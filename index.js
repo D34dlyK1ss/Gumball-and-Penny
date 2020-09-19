d78e3c1baea3f227a4c7215725c24922beb76126
D34dlyK1ss <rags.ftjs@gmail.com>
2020-09-19 23:31:46 +0100

    Updated

--- index.js
+++ index.js
@@ -64,6 +64,28 @@ bot.once('ready', async () => {
 	});
 });
 
+const express = require('express');
+const http = require('http');
+const WebSocket = require('ws');
+
+const port = 5000;
+const server = http.createServer(express);
+const wss = new WebSocket.Server({ server });
+
+wss.on('connection', function connection(ws) {
+	ws.on('message', function incoming(data) {
+		wss.clients.forEach(function each(client) {
+			if (client !== ws && client.readyState === WebSocket.OPEN) {
+				client.send(data);
+			}
+		});
+	});
+});
+
+server.listen(port, function() {
+	console.log(`Servidor está ativo na porta ${port}!`);
+});
+
 const DBL = require('dblapi.js');
 const dbl = new DBL(config.dblToken, { webhookPort: 5000, webhookAuth: 'gumballandpenny' });
 dbl.webhook.on('vote', vote => {
