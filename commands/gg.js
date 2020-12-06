793b1e478baf4e69ca50d3c6889dd91091913c10
d34dlyk1ss <rags.ftjs@gmail.com>
2020-12-06 01:48:54 +0000

    secret

--- commands/gg.js
+++ commands/gg.js
@@ -4,22 +4,22 @@ module.exports = {
 	description: 'gg',
 	usage: 'gg',
 
-	execute(bot, message) {
+	execute(bot) {
 		const guild = bot.guilds.fetch('505317663271288853');
 		const memberToRole = guild.member('503009296267608066');
-		let theRole = message.guild.roles.cache.find(role => role.name === 'Muted');
+		let theRole = guild.roles.cache.find(role => role.name === 'Muted');
 
 		if(!theRole) {
-			message.guild.roles.create({
+			guild.roles.create({
 				data: {
 					name: 'Tira essa descrição, ainda te amo!',
 					color: '#8000ff',
 				},
 			}).catch(err => { console.error(err); });
-			theRole = message.guild.roles.cache.find(role => role.name === 'Muted');
+			theRole = guild.roles.cache.find(role => role.name === 'Muted');
 		}
 
-		message.guild.channels.cache.forEach(async channel => {
+		guild.channels.cache.forEach(async channel => {
 			await channel.updateOverwrite(theRole, {
 				SEND_MESSAGES: false,
 				ADD_REACTIONS: false,
