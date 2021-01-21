013107070152d2002c4226150fd45a71aa0c31ae
d34dlyk1ss <rags.ftjs@gmail.com>
2021-01-21 14:00:26 +0000

    minor fix

--- commands/clear.js
+++ commands/clear.js
@@ -3,29 +3,26 @@ module.exports = {
 
 	execute(bot, message, command, db, lang, language, supportServer, prefix, args) {
 		message.delete().then(() => {
+			let number = parseInt(args);
+
 			if (!message.member.hasPermission('MANAGE_MESSAGES')) {
 				return message.reply(lang.error.noPerm).catch(err => { console.error(err); });
 			}
 			else if (!message.channel.guild.me.hasPermission('MANAGE_MESSAGES')) {
 				return message.reply(lang.error.botNoManageMsgs).then(msg => msg.delete({ timeout: 5000 })).catch(err => { console.error(err); });
 			}
-			else if (!args || args == '0') {
+			else if (args == '' || args == '0' || !Number.isInteger(number)) {
 				return message.reply(lang.error.noNumberToDelete).then(msg => msg.delete({ timeout: 5000 })).catch(err => { console.error(err); });
 			}
 			else {
-				let number = parseInt(args),
-					plural;
+				let plural;
 
-				if (!Number.isInteger(number)) {
-					return;
-				}
-				else {
-					if (number > 100) number = 100;
-					message.channel.bulkDelete(number, true).then(deleted => {
-						deleted.size != 1 ? plural = lang.clear.messages : plural = lang.clear.message;
-						message.channel.send(`${lang.clear.weDeleted}\`${deleted.size}\`${plural}!`).then(msg => msg.delete({ timeout: 5000 })).catch(err => { console.error(err); });
-					});
-				}
+				if (number > 100) number = 100;
+
+				message.channel.bulkDelete(number, true).then(deleted => {
+					deleted.size != 1 ? plural = lang.clear.messages : plural = lang.clear.message;
+					message.channel.send(`${lang.clear.weDeleted}\`${deleted.size}\`${plural}!`).then(msg => msg.delete({ timeout: 5000 })).catch(err => { console.error(err); });
+				});
 			}
 		});
 	},
