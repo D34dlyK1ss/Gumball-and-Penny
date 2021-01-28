5a0262a9ceaae05a0f60026ce15995dca456655a
d34dlyk1ss <rags.ftjs@gmail.com>
2021-01-28 14:41:32 +0000

    minor change

--- bot.js
+++ bot.js
@@ -58,6 +58,9 @@ const mili = currentdate - relationship;
 
 const years = Math.round(mili / 31536000000);
 const months = Math.round(mili / 2629746000) - (12 * years);
+let plural;
+
+years > 1 ? plural = 's' : '';
 
 // Uma vez que o bot está ativo:
 bot.once('ready', async () => {
@@ -70,8 +73,8 @@ bot.once('ready', async () => {
 	}, 1800000);
 
 	schedule.scheduleJob('0 14 28 * *', function() {
-		bot.users.resolve(config.lilly).send(`:tada: Parabéns Lilly! Completaste ${years} anos e ${months} meses com o teu Ruru! :purple_heart:\nhttps://i.imgur.com/clrwrEk.gif`);
-		bot.users.resolve(config.botOwner).send(`:tada: Parabéns Ruru! Completaste ${years} anos e ${months} meses com a tua Lilly! :purple_heart:\nhttps://i.imgur.com/clrwrEk.gif`);
+		bot.users.resolve(config.lilly).send(`:tada: Parabéns Lilly! Completaste ${years} ano${plural} e ${months} meses com o teu Ruru! :purple_heart:\nhttps://i.imgur.com/clrwrEk.gif`);
+		bot.users.resolve(config.botOwner).send(`:tada: Parabéns Ruru! Completaste ${years} ano${plural} e ${months} meses com a tua Lilly! :purple_heart:\nhttps://i.imgur.com/clrwrEk.gif`);
 	});
 });
 
