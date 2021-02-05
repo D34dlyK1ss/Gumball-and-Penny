eb18197974303e629ce141b4ffb3c6c438b538c5
d34dlyk1ss <rags.ftjs@gmail.com>
2021-02-05 21:09:10 +0000

    added the new command to help

--- commands/help.js
+++ commands/help.js
@@ -20,7 +20,7 @@ module.exports = {
 				{ name: `⚠️ ${lang.moderation}`, value: '`ban`, `clear`, `kick`, `mute`, `tempmute`, `unmute`', inline: true },
 				{ name: `🌐 ${lang.server}`, value: '`members`, `serverinfo`, `userinfo`', inline: true },
 				{ name: `🛠️ ${lang.utility}`, value: '`avatar`, `invite`, `ping`', inline: true },
-				{ name: `⚙️ ${lang.settings}`, value: '`setlanguage`, `setprefix`', inline: true },
+				{ name: `⚙️ ${lang.settings}`, value: '`automessages`, `setlanguage`, `setprefix`', inline: true },
 				{ name: `${lang.links}`, value: `**[${lang.inviteUs}](https://discordapp.com/oauth2/authorize?&client_id=679041548955942914&scope=bot&permissions=272100438) - [${lang.supportServer}](https://discord.gg/FaUGnB25hF) - [${lang.donate}](https://ko-fi.com/officialgumballandpenny/commissions)**` },
 			);
 
