df87867405108d26098ea7579c7a9406ac132549
d34dlyk1ss <rags.ftjs@gmail.com>
2021-02-10 23:21:06 +0000

    now I can check other servers with that command

--- commands/serverinfo.js
+++ commands/serverinfo.js
@@ -1,25 +1,31 @@
 const Discord = require('discord.js');
+const config = require('../config.json');
 const moment = require('moment');
 
 module.exports = {
 	name: 'serverinfo',
 	aliases: ['si'],
 
-	execute(bot, message, command, db, lang, language) {
-		const server = message.guild,
-			createdDate = moment(server.createdAt).locale(`${language}`);
-		const embed = new Discord.MessageEmbed()
-			.setColor('#8000ff')
-			.setAuthor(`${server.name}`, `${server.iconURL()}`)
-			.setThumbnail(`${server.iconURL()}`)
-			.addFields(
-				{ name: `${lang.id}`, value: `${server.id}` },
-				{ name: `${lang.verificationLevel}`, value: `${lang.serverinfo.verificationLevel[server.verificationLevel]}`, inline: true },
-				{ name: `${lang.region}`, value: `${lang.serverinfo.region[server.region]}`, inline: true },
-				{ name: `${lang.membersServerInfo}`, value: `${server.memberCount}`, inline: true },
-				{ name: `${lang.creation}`, value: `${lang.created + createdDate.format('LLL')}` },
-				{ name: `${lang.owner}`, value: `${server.owner}`, inline: true },
-			);
+	execute(bot, message, command, db, lang, language, supportServer, prefix, args) {
+		let server = message.guild;
+
+		if (args != '' && message.author == config.botOwner) {
+			server = bot.guilds.cache.get(args[0]);
+		}
+
+		const createdDate = moment(server.createdAt).locale(`${language}`),
+			embed = new Discord.MessageEmbed()
+				.setColor('#8000ff')
+				.setAuthor(`${server.name}`, `${server.iconURL()}`)
+				.setThumbnail(`${server.iconURL()}`)
+				.addFields(
+					{ name: `${lang.id}`, value: `${server.id}` },
+					{ name: `${lang.verificationLevel}`, value: `${lang.serverinfo.verificationLevel[server.verificationLevel]}`, inline: true },
+					{ name: `${lang.region}`, value: `${lang.serverinfo.region[server.region]}`, inline: true },
+					{ name: `${lang.membersServerInfo}`, value: `${server.memberCount}`, inline: true },
+					{ name: `${lang.creation}`, value: `${lang.created + createdDate.format('LLL')}` },
+					{ name: `${lang.owner}`, value: `${server.owner}`, inline: true },
+				);
 
 		if (!server.iconURL()) {
 			const lastEmbed = embed;
