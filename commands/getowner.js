93ed59317ef6ea8c947af7a2e9da26f4fd62baca
d34dlyk1ss <rags.ftjs@gmail.com>
2021-02-11 00:24:44 +0000

    minor change

--- commands/getowner.js
+++ commands/getowner.js
@@ -4,8 +4,12 @@ module.exports = {
 	name: 'getowner',
 
 	async execute(bot, message, command, db, lang, language, supportServer, prefix, args) {
-		if(message.author != config.botOwner) return;
+		if(message.author.id !== config.botOwner || message.channel.id !== '809182965607039007') return;
+
 		const server = await bot.guilds.cache.get(args[0]);
-		message.channel.send(server.owner.id);
+
+		if (!server) return message.channel.send('O bot não está no servidor ou então não está na cache!').catch(err => { console.error(err); });
+
+		message.channel.send(server.owner.id).catch(err => { console.error(err); });
 	},
 };
