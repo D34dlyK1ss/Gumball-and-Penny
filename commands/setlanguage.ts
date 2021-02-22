9ba0187fae1a87b64062e53339f22580ef739ab5
d34dlyk1ss <rags.ftjs@gmail.com>
2021-02-22 12:42:55 +0000

    fixed setlanguage

--- commands/setlanguage.ts
+++ commands/setlanguage.ts
@@ -13,28 +13,31 @@ export function execute(bot: undefined, message: Message, command: undefined, db
 			newLanguage = args[0].toLowerCase(),
 			ref = db.collection('definicoes').doc(message.guild.id);
 
-		if (!availableLangs.includes(newLanguage)) message.reply(`${lang.setlanguage.noLanguage}\`${prefix}help setlanguage\`${lang.forMoreInfo}`).catch(err => { console.error(err); });
+		if (!availableLangs.includes(newLanguage)) {
+			message.reply(`${lang.setlanguage.noLanguage}\`${prefix}help setlanguage\`${lang.forMoreInfo}`).catch(err => { console.error(err); });
+		}
+		else {
+			ref.get().then((doc: any) => {
+				if (!doc.exists) {
+					ref.set({
+						settings: botConfig.settings,
+					}, { merge: true }).catch((err: Error) => { console.error(err); });
+				}
 
-		ref.get().then((doc: any) => {
-			if (!doc.exists) {
-				ref.set({
-					settings: botConfig.settings,
-				}, { merge: true }).catch((err: Error) => { console.error(err); });
-			}
+				const oldLanguage = doc.get('settings').language || botConfig.settings.language;
 
-			const oldLanguage = doc.get('settings').language || botConfig.settings.language;
-
-			if (newLanguage == oldLanguage) {
-				message.reply(lang.error.sameLanguage).catch(err => { console.error(err); });
-			}
-			else {
-				serverSettings.language = newLanguage;
-				ref.set({
-					settings: { 'language': newLanguage },
-				}, { merge: true }).catch((err: Error) => { console.error(err); });
-			}
-			lang = require(`../lang/${newLanguage}.json`);
-			message.channel.send(`${lang.setlanguage.isNow}`).catch(err => { console.error(err); });
-		});
+				if (newLanguage == oldLanguage) {
+					message.reply(lang.error.sameLanguage).catch(err => { console.error(err); });
+				}
+				else {
+					serverSettings.language = newLanguage;
+					ref.set({
+						settings: { 'language': newLanguage },
+					}, { merge: true }).catch((err: Error) => { console.error(err); });
+				}
+				lang = require(`../lang/${newLanguage}.json`);
+				message.channel.send(`${lang.setlanguage.isNow}`).catch(err => { console.error(err); });
+			});
+		}
 	}
 };
