09710c0848baedf1acc7f47d0dc837fc5fbf6c0c
D34dlyK1ss <48548823+D34dlyK1ss@users.noreply.github.com>
2021-06-11 22:39:16 +0100

    Update fact.ts

--- commands/fact.ts
+++ commands/fact.ts
@@ -2,7 +2,7 @@ import { Message } from 'discord.js';
 
 export const name = 'fact';
 export function execute(bot: undefined, message: Message, command: undefined, db: undefined, lang: undefined, language: string) {
-	const rnd = Math.floor(Math.random() * 24),
+	const rnd = Math.floor(Math.random() * 22 + 1),
 		facts = require('../src/data/facts.json');
 
 	message.channel.send(facts[language][rnd]).catch(err => { console.error(err); });
