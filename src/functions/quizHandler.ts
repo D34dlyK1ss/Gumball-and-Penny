439ea0e6de142f89c356bb2df0f8806530c7a9ff
D34dlyK1ss <rags.ftjs@gmail.com>
2021-09-17 20:59:50 +0100

    fixed

--- src/functions/quizHandler.ts
+++ src/functions/quizHandler.ts
@@ -169,18 +169,20 @@ export function createQuizQuestion(interaction: ButtonInteraction, user: User, l
 										if (score.size !== 0)
 										{
 											const nParticipants = score.size;
+											const userId = score.keys();
+											const points = score.values();
 											let description = '';
 											
+
+
 											for (let j = 0; j < nParticipants; j++) {
-												const userId = score.keys().next().value;
-												const points = score.values().next().value;
-												const refP = admin.firestore().collection('perfis').doc(userId);
-												description = description + `\n<@${userId}> - ${points} ${lang.quiz.points}`;
+												const refP = admin.firestore().collection('perfis').doc(userId.next().value);
+												description = description + `\n<@${userId.next().value}> - ${points.next().value} ${lang.quiz.points}`;
 
 												await refP.get().then(doc => {
 													if (!doc.exists) return;
 													const xp: number = doc.get('xp');
-													const xpGain = 25 * points + 50 * (nParticipants - 1);
+													const xpGain = 25 * points.next().value + 50 * (nParticipants - 1);
 
 													refP.update({
 														xp: xp + xpGain
