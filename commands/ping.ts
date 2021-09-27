e1c6ae2246b0ae697cf3cbfd59ef5e1948779fdd
D34dlyK1ss <rags.ftjs@gmail.com>
2021-09-27 10:51:19 +0100

    improved ping command

--- commands/ping.ts
+++ commands/ping.ts
@@ -1,13 +1,14 @@
 import { Message, MessageEmbed } from 'discord.js';
+import { BotClient } from '../indexDev';
 
 export const name = 'ping';
-export function execute(bot: undefined, message: Message, command: undefined, db: undefined, lang: Record<string, string | any>) {
-	message.reply('Pong!').then(msg => {
-		const ping = msg.createdTimestamp - message.createdTimestamp;
-		const embed = new MessageEmbed()
-			.setAuthor(`${lang.ping.yourPingIs}${ping}ms`)
-			.setColor('DARK_PURPLE');
+export function execute(bot: BotClient, message: Message, command: undefined, db: undefined, lang: Record<string, string | any>) {
+	const embed = new MessageEmbed()
+		.addFields([
+			{ name: 'Ping', value: `${Date.now() - message.createdTimestamp}ms.` },
+			{ name: lang.ping.apiPing, value: `${bot.ws.ping}ms.` }
+		])
+		.setColor('DARK_PURPLE');
 
-		msg.edit({ embeds: [embed] });
-	}).catch(err => { console.error(err); });
+	message.reply({ content: 'Pong! 🏓', embeds: [embed] }).catch(err => { console.error(err); });
 }
