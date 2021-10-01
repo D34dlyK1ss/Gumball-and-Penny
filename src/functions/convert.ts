0da10a0ad08d9b3a404bf5f04e5fef9debdf8840
D34dlyK1ss <rags.ftjs@gmail.com>
2021-10-01 15:38:12 +0100

    tweaks

--- src/functions/convert.ts
+++ src/functions/convert.ts
@@ -1,12 +1,8 @@
 function convert(value: number) {
 	let valueString = value.toString();
 
-	if (value >= 1000000) {
-		valueString = `${value / 1000000}M`;
-	}
-	else if (value >= 1000) {
-		valueString = `${value / 1000}k`;
-	}
+	if (value >= 1000000) valueString = `${value / 1000000}M`;
+	else if (value >= 1000) valueString = `${value / 1000}k`;
 
 	return valueString;
 }
--- src/functions/titleCase.ts
+++ src/functions/titleCase.ts
@@ -4,6 +4,7 @@ function titleCase(str: string) {
 	for (let i = 0; i < splitStr.length; i++) {
 		splitStr[i] = splitStr[i].charAt(0).toUpperCase() + splitStr[i].substring(1);
 	}
+
 	return splitStr.join(' ');
 }
 
