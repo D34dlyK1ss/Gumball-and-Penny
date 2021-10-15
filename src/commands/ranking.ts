8c63786093bf18723fe88280f11a1af02bc1ee24
D34dlyK1ss <rags.ftjs@mail.com>
2021-10-15 04:21:32 -0700

    reworked ranking (not tested)

--- src/commands/ranking.ts
+++ src/commands/ranking.ts
@@ -7,7 +7,8 @@ export const aliases = ['leaderboard'];
 
 let lastUpdateAt = 0;
 const rankingEmbed = new MessageEmbed()
-	.setColor('DARK_PURPLE');
+	.setColor('DARK_PURPLE')
+	.setThumbnail('https://i.imgur.com/0lJXooH.png');
 
 export async function execute(bot: undefined, message: Message, command: undefined, db: FirebaseFirestore.Firestore, lang: Record<string, string | any>, language: string) {
 	moment.locale(language);
@@ -15,20 +16,26 @@ export async function execute(bot: undefined, message: Message, command: undefin
 	const refP = db.collection('perfis');
 	const query = await refP.orderBy('xp', 'desc').limit(10).get();
 	let i = 0;
+	let column = '';
+	let column2 = '';
 
 	if (Date.now() - lastUpdateAt > 1800000) {
 		rankingEmbed.spliceFields(0, 10);
 		query.forEach(doc => {
 			i++;
-			rankingEmbed.addField(`\n${i}# ${doc.get('name')}`, `${lang.level} ${doc.get('level')}, ${doc.get('xp')} XP`);
+			column = `\n${i}# ${doc.get('name')}`;
+			column2 = `\n${lang.level} ${doc.get('level')}, ${doc.get('xp')} XP`;
+		}).then(() => {
+			lastUpdateAt = Date.now();
+
+			rankingEmbed
+				.addFields([
+					{ name: lang.ranking.title, value: column, inline: true },
+					{ name: 'XP', value: column2, inline: true },
+				])
+				.setFooter(getText(lang.ranking.updatedAt, [moment(lastUpdateAt).utc().format('LL'), moment(lastUpdateAt).utc().format('LTS')]));
 		});
-
-		lastUpdateAt = Date.now();
-		rankingEmbed.setFooter(getText(lang.ranking.updatedAt, [moment(lastUpdateAt).utc().format('LL'), moment(lastUpdateAt).utc().format('LTS')]));
 	}
-
-	rankingEmbed.setTitle(lang.ranking.title)
-		.setDescription(lang.ranking.description);
 
 	message.channel.send({ embeds: [rankingEmbed] });
 }
