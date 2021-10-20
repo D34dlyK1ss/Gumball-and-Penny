0590a5dbde352661dc1c5ccb0e60fd0ae42a56e0
D34dlyK1ss <rags.ftjs@mail.com>
2021-10-20 03:26:34 -0700

    refactored

--- src/functions/giveVoteAwards.ts
+++ src/functions/giveVoteAwards.ts
@@ -1,23 +1,19 @@
-import botConfig from 'botConfig.json';
+import botConfig from '../../botConfig.json';
 
 async function giveVoteAwards(db: FirebaseFirestore.Firestore, vips: Set<string>, userID: string) {
 	const refP = db.collection('perfis').doc(userID);
 
-	await refP.get().then(doc => {
-		if (!doc.exists) return;
-		const bal: number = doc.get('balance');
+	await refP.get().then(docP => {
+		if (!docP.exists) return;
+		const bal: number = docP.get('balance');
 		let add = 150;
 
 		if (userID === botConfig.botOwnerID || botConfig.collaboratorIDs.includes(userID) || vips.has(userID)) {
 			add *= 2;
 		}
 
-		refP.get().then(docP => {
-			if (docP.exists) {
-				refP.update({
-					balance: bal + add
-				});
-			}
+		refP.update({
+			balance: bal + add
 		});
 	});
 }
