9ae3152a38fca423e50a4f4ef1321dee6dc6861b
D34dlyK1ss <48548823+D34dlyK1ss@users.noreply.github.com>
2021-11-08 13:29:45 +0000

    Update help.ts

--- src/commands/help.ts
+++ src/commands/help.ts
@@ -20,7 +20,7 @@ export function execute(bot: BotClient, message: Message, command: Cmd, db: unde
 			{ name: `🌐 ${lang.server}`, value: '`members`, `serverinfo`, `userinfo`', inline: true },
 			{ name: `🛠️ ${lang.utility}`, value: '`avatar`, `invite`, `ping`, `vip`, `vote`', inline: true },
 			{ name: `⚙️ ${lang.settings}`, value: '`automessages`, `setlanguage`, `setprefix`', inline: true },
-			{ name: `${lang.links}`, value: `**[${lang.inviteUs}](https://discordapp.com/oauth2/authorize?&client_id=679041548955942914&scope=bot&permissions=272100438) - [${lang.supportServer}](https://discord.gg/FaUGnB25hF) - [${lang.buyVIP}](https://www.patreon.com/suicidekiss)**` }
+			{ name: `${lang.links}`, value: `**[${lang.inviteUs}](https://discordapp.com/oauth2/authorize?&client_id=679041548955942914&scope=bot&permissions=272100438) - [${lang.buyVIP}](https://www.patreon.com/suicidekiss)**` }
 		);
 
 	if (!args[0]) {
