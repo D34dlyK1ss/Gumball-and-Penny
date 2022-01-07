aa051a4675af7eda7c5bc069aea155b582baad37
D34dlyK1ss <rags.ftjs@gmail.com>
2022-01-07 16:24:43 +0000

    change to invite

--- src/commands/invite.ts
+++ src/commands/invite.ts
@@ -3,5 +3,5 @@ import { BotClient } from 'index';
 
 export const name = 'invite';
 export function execute(bot: BotClient, message: Message, command: undefined, db: undefined, lang: Record<string, string | any>) {
-	message.channel.send(`${lang.inviteUsToYourServer}\nhttps://discord.com/api/oauth2/authorize?client_id=${bot.user.id}&permissions=1239370034246&scope=bot`);
+	message.channel.send(`${lang.inviteUsToYourServer}\nhttps://discord.com/api/oauth2/authorize?client_id=${bot.user.id}&permissions=1237222550598&scope=bot`);
 }
