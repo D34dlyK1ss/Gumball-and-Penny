95272bc5387686740bf65ce81f5dc79115fe177f
D34dlyK1ss <rags.ftjs@gmail.com>
2022-01-07 11:14:53 +0000

    brought VIP back

--- src/commands/vip.ts
+++ src/commands/vip.ts
@@ -1,14 +1,13 @@
-import { Message/*, MessageActionRow, MessageButton*/ } from 'discord.js';
+import { Message, MessageActionRow, MessageButton } from 'discord.js';
 
 export const name = 'vip';
 export function execute(bot: undefined, message: Message, command: undefined, db: undefined, lang: Record<string, string | any>) {
-	/*const row = new MessageActionRow().addComponents(
+	const row = new MessageActionRow().addComponents(
 		new MessageButton()
 			.setLabel('Link')
 			.setStyle('LINK')
 			.setURL('https://www.patreon.com/suicidekiss')
 	);
 
-	message.channel.send({ content: lang.vip.click, components: [row] });*/
-	message.channel.send(lang.vip.deprecated);
+	message.channel.send({ content: lang.vip.click, components: [row] });
 }
