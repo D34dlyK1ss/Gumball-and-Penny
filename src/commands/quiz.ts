f94a98c3a75a55f45a00ef020a3c976ee69aba4c
D34dlyK1ss <rags.ftjs@gmail.com>
2022-08-04 11:33:56 +0100

    disabled quiz

--- src/commands/quiz.ts
+++ src/commands/quiz.ts
@@ -8,7 +8,9 @@ export = {
 		.setDescription(enLang.command.quiz.description),
 
 	async execute(bot: undefined, interaction: ChatInputCommandInteraction, db: FirebaseFirestore.Firestore, lang: Record<string, any>) {
-		if (alreadyPlaying.has(interaction.channelId)) {
+		await interaction.editReply('This command is disabled while our creators work on it!');
+
+		/*if (alreadyPlaying.has(interaction.channelId)) {
 			interaction.reply(lang.command.quiz.alreadyPlaying);
 		}
 		else {
@@ -17,6 +19,6 @@ export = {
 			const page = createQuizPage(interaction, interaction.user, lang, 'quizmainEmbed');
 		
 			await interaction.editReply({ embeds: [page.embed], components: [page.buttonRow] });
-		}
+		}*/
 	}
 };
